14acb23b02e0a541913980dba7b3c215e8bc88cb
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2024-03-03 14:56:09 -0600

    Added to_string method for both text and topic questions for debugging purposes

--- text_questions.py
+++ text_questions.py
@@ -3,14 +3,23 @@ import google.generativeai as genai
 
 # input text from source to create questions
 class TextQuestions:
+    # constructor
     def __init__(self, text_src):
         # Gemini model and chat initialization
         self.model = genai.GenerativeModel('gemini-1.0-pro-001')
-        self.mpc_text_chat = self.model.start_chat(history=[])
+        self.text_chat = self.model.start_chat(history=[])
 
         # text src and question initialization
         self.text_src = text_src
         self.current_question_number = 1
 
-
+    # to_string method for debugging purposes
+    def __str__(self):
+        return_string = ""
+        # count = 0
+        for message in self.text_chat.history:
+            # if count % 2 != 0:
+            return_string += f"\nNew\n\n{message.parts[0].text}End"
+            # count += 1
+        return return_string
 
--- topic_questions.py
+++ topic_questions.py
@@ -2,6 +2,7 @@ import google.generativeai as genai
 
 
 class TopicQuestions:
+    # constructor
     def __init__(self, topic):
         # Gemini and chat initialization
         self.model = genai.GenerativeModel('gemini-1.0-pro-001')
@@ -11,3 +12,12 @@ class TopicQuestions:
         self.topic = topic
         self.current_question_number = 1
 
+    # to_string method for debugging purposes
+    def __str__(self):
+        return_string = ""
+        # count = 0
+        for message in self.topic_chat.history:
+            # if count % 2 != 0:
+            return_string += f"\nNew\n\n{message.parts[0].text}End"
+            # count += 1
+        return return_string
