5a7a92499d5d154e9365a6a9af4b99391113d2f5
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2024-03-15 03:10:40 -0500

    Added a home page and added path/urls to Exam app and AI_Exam project directory

--- Exam/views.py
+++ Exam/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    # You can add data to be passed to the template here (optional)
+    context = {}
+    return render(request, 'home.html', context)
+
+
